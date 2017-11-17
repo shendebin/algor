a6dc60b297a6f3bf4b228c1dcee270687733c50d
shen <shen4399@gmail.com>
2017-11-17 18:47:14 +0800

    test

--- src/main/java/com/shen/algor/queue/PriorityQueue.java
+++ src/main/java/com/shen/algor/queue/PriorityQueue.java
@@ -15,12 +15,38 @@ public class PriorityQueue {
         this.nItems = 0;
     }
 
-
     public void insert(int item){
-        //TODO
+        int j;
+
+        if (nItems == 0){
+            queArray[nItems ++] = item;
+        }else {
+            for (j = nItems-1; j >= 0; j--){
+                if(item > queArray[j]){
+                    queArray[j+1] = queArray[j];
+                }else {
+                    break;
+                }
+                queArray[j+1] = item;
+                nItems ++;
+            }
+        }
     }
 
-    public void remove(){
-        //TODO
+    public int remove(){
+        return queArray[--nItems];
     }
+
+    public int peekMin(){
+        return queArray[nItems-1];
+    }
+
+    public boolean isEmpty(){
+        return nItems == 0;
+    }
+
+    public boolean isFull(){
+        return nItems == maxSize;
+    }
+
 }
